4d60c2330510ff603890e1164afe703704710d68
xhitz <heylistenplz@gmail.com>
2022-09-13 21:14:37 +0100

    added modifies

--- Contracts/MultiSigWallet.sol
+++ Contracts/MultiSigWallet.sol
@@ -22,6 +22,26 @@ contact MultiSigWallet {
     Transaction[] public transactions;
     mapping(uint => mapping(address => bool)) public approved;
 
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint txId) {
+        require(txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notApproved(uint txId) {
+        require(!approved[txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint txId) {
+        require(!transactions[txId].executed, "tx already executed");
+        _;
+    }
+
     constructor(address[] memory _owners, uint _required) {
         require(_owners.length > 0, "owners required");
         require(_required > 0 && _required <= _owners.length, "invalid required number of owners");
@@ -44,7 +64,7 @@ contact MultiSigWallet {
         }
     }
 
-    function submitTransaction(address to, uint value, bytes memory data) public returns (uint txId) {
+    function submit(address to, uint value, bytes memory data) public returns (uint txId) {
         require(isOwner[msg.sender], "not owner");
         txId = transactions.length;
         transactions.push(Transaction({
@@ -53,9 +73,52 @@ contact MultiSigWallet {
             data: data,
             executed: false
         }));
-        emit Submit(txId);
+        emit Submit(transactions.length - 1);
     }
 
+    function approve(uint txId)
+        external
+        onlyOwner
+        txExists(txId)
+        notExecuted(txId)
+        notApproved(txId)
+    {
+        approved[txId][msg.sender] = true;
+        emit Approve(msg.sender, txId);
+        execute(txId);
+    }
+
+    function _getApprovalCount(uint txId) internal view returns (uint count) {
+        for (uint i = 0; i < owners.length; i++) {
+            if (approved[txId][owners[i]]) {
+                count += 1;
+            }
+        }
+    }
+
+    function execute(uint txId) external txExists(txId) notExecuted(txID) {
+        require(_getApprovalCount(txId) >= required, "approvals < required");
+        Transaction storage transaction = transactions[txId];
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(transaction.data);
+        require(success, "tx failed");
+
+        emit Execute(txId);
+    }
+    
+    function revoke(uint txId)
+        external
+        onlyOwner
+        txExists(txId)
+        notExecuted(txId)
+        notApproved(txId)
+    {
+        requre(approved[txId][msg.sender], "tx not approved");
+        approved[txId][msg.sender] = false;
+        emit Revoke(msg.sender, txId);
+    }
 }
 
 
