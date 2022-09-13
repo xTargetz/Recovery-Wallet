82b873e15e6062d671e4d4ba530e1b164579aa39
xhitz <heylistenplz@gmail.com>
2022-09-13 18:20:27 +0100

    Create MultiSigWallet Template

---
+++ Contracts/MultiSigWallet.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contact MultiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event Approve(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txID);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public required;
+
+    Transaction[] public transactions;
+    mapping(uint => mapping(address => bool))
+
+    
+
+
+}
+
+// create a game with 2 players
+
