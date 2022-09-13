562f5dca4b9f0eb710f0c21ca5c48306c5175273
xhitz <heylistenplz@gmail.com>
2022-09-13 18:27:04 +0100

    modified

--- Contracts/MultiSigWallet.sol
+++ Contracts/MultiSigWallet.sol
@@ -20,12 +20,43 @@ contact MultiSigWallet {
     uint public required;
 
     Transaction[] public transactions;
-    mapping(uint => mapping(address => bool))
+    mapping(uint => mapping(address => bool)) public approved;
 
-    
+    constructor(address[] memory _owners, uint _required) {
+        require(_owners.length > 0, "owners required");
+        require(_required > 0 && _required <= _owners.length, "invalid required number of owners");
 
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        required = _required;
+    }
+
+    receive() external payable {
+        if (msg.value > 0) {
+            emit Deposit(msg.sender, msg.value);
+        }
+    }
+
+    function submitTransaction(address to, uint value, bytes memory data) public returns (uint txId) {
+        require(isOwner[msg.sender], "not owner");
+        txId = transactions.length;
+        transactions.push(Transaction({
+            to: to,
+            value: value,
+            data: data,
+            executed: false
+        }));
+        emit Submit(txId);
+    }
 
 }
 
-// create a game with 2 players
+
 
