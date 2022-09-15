da025e354f25d78ec6b24e6d4c81ed6b071ec903
stereoIII6 <stereodocbush@gmail.com>
2022-09-15 16:09:14 +0200

    update safes

--- Contracts/SafeFactory.sol
+++ Contracts/SafeFactory.sol
@@ -1,20 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 import "./MultiSigWallet.sol";
+
 contract SafeFactory {
     MultiSigWallet public multi;
+
     mapping(uint256 => address) public safeAdr;
     mapping(uint256 => address) public adminAdr;
+    mapping(address => uint256) public safeCount;
+    mapping(address => mapping(uint256 => address)) public mySafes;
     uint256 s;
+    address admin;
 
-    constructor(){
+    constructor() {
+        admin = msg.sender;
         s = 0;
     }
-    function createSafe(address[] memory _adrs, uint256 _req, address _admin) external returns(bool){
+
+    function createSafe(
+        address[] memory _adrs,
+        uint256 _req,
+        address _admin
+    ) external returns (bool) {
         multi = new MultiSigWallet(_adrs, _req, _admin);
         safeAdr[s] = address(multi);
         adminAdr[s] = msg.sender;
+        mySafes[_admin][safeCount[_admin]] = safeAdr[s];
+        safeCount[_admin]++;
         s++;
         return true;
     }
 }
+
